c0942b5a334e33c69fb84d44793aa7d5fca3bb06
BobbyKataria <bobby.kataria@hotmail.co.uk>
2019-07-08 16:21:39 +0000

    working

--- install.sh
+++ install.sh
@@ -6,10 +6,20 @@ sudo apt-get install git
 
 #get the latest version of nodejs
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - 
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 #cloning all the repos
+
+git clone https://github.com/Nboaram/TeamAPoolProjectUI.git
+
+cd TeamAPoolProjectUI/
+
+git checkout Developer
+
+yes | sudo npm install -y
+
+cd ../
 
 git clone https://github.com/Nboaram/TeamAPoolProjectBackend.git
 
@@ -21,23 +31,15 @@ sudo npm install
 
 sudo apt install -y mongodb
 
+cd ../
+
 sudo cp mongodb.service /etc/systemd/system/
-        
+
 sudo systemctl daemon-reload
-             
+
 sudo systemctl start mongodb
 
 exit
-
-cd ../ 
-
-git clone https://github.com/Nboaram/TeamAPoolProjectUI.git
-
-cd TeamAPoolProjectUI/
-
-git checkout Developer
-
-sudo npm install -y
 
 yes | sudo npm install -g @angular/cli
 
