70bb815ef24a54af170fcd0a19d488bf0ef39666
BobbyKataria <bobby.kataria@hotmail.co.uk>
2019-07-09 17:00:22 +0100

    updated

--- vmsetup.sh
+++ vmsetup.sh
@@ -1,15 +1,22 @@
 #creating our front end and back end server
 
-gcloud compute instances create meanvm --image ubuntu-1804-bionic-v20190628 --image-project ubuntu-os-cloud --tags front --zone europe-west2-c
+#gcloud compute instances create meanvm --image ubuntu-1804-bionic-v20190628 --image-project ubuntu-os-cloud --tags front --zone europe-west2-c
 
 #creating our firewall rules
-gcloud compute firewall-rules create frontend --allow tcp:4200 --target-tags front --description="Creating the port for our frontend using 4200"
+#gcloud compute firewall-rules create frontend --allow tcp:4200 --target-tags front --description="Creating the port for our frontend using 4200"
 
-gcloud compute firewall-rules create backend --allow tcp:8080 --target-tags front --description="Creating the port for our backend using 8080"
+#gcloud compute firewall-rules create backend --allow tcp:8080 --target-tags front --description="Creating the port for our backend using 8080"
 
-#ssh into our vm's
+#run commands through ssh in our vm
 
-gcloud compute ssh meanvm
+#gcloud compute ssh meanvm --command "git clone https://github.com/BobbyKataria/poolappScript.git"
 
-#git clone
-git clone https://github.com/BobbyKataria/poolappScript.git
+gcloud compute ssh meanvm --command "cd poolappScript && ./install.sh"
+
+#gcloud compute ssh meanvm --command "./install.sh"
+
+
+
+
+
+
