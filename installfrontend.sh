e88d2f970d6d1338906a81f346f15df1f44b97fa
Bobby Kataria <bobby.kataria@hotmail.co.uk>
2019-07-12 12:32:56 +0000

    updated for adminbk user instead of asianguass

--- installfrontend.sh
+++ installfrontend.sh
@@ -3,14 +3,13 @@
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt-get install git
-
+sudo cp ng.service /etc/systemd/system
 #get the latest version of nodejs
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 #cloning all the repos
-cd ~
 rm -rf TeamAPoolProjectUI
 git clone https://github.com/Nboaram/TeamAPoolProjectUI.git
 
@@ -24,7 +23,6 @@ sudo NG_CLI_ANALYTICS=ci npm install -g @angular/cli
 
 cd ../
 
-sudo cp ng.service /etc/systemd/system
 
 sudo systemctl daemon-reload
 sudo systemctl restart ng.service
